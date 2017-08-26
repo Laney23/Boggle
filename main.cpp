07a95760eaff721f319524a4f8a121e6acbbc461
Laney23 <laney.25@osu.edu>
2017-08-26 12:25:18 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  boggle
+//
+//  Created by Josh Laney on 8/26/17.
+//  Copyright © 2017 Aginor. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
